eb1985f1858fc13d0b734cb14da3fc5a44a353ae
blertton <blettonkr@gmail.com>
2023-12-05 13:08:56 +0100

    TypeScript data

---
+++ monorepo/TypeScript/types.d.ts
@@ -0,0 +1,13 @@
+interface Intern {
+    name: string;
+    age: number;
+    skills: ["React", "HTML", "JS", "TypeScript"] ;
+}
+
+interface Junior {
+    name: string;
+    age: number;
+    skills: string[];
+    date_of_promotion: string;
+}
+
